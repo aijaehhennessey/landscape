c1271fc5c6a538b7edb93e56931908bedd5eb55c
aijaehhennessey <60896810+aijaehhennessey@users.noreply.github.com>
2020-02-23 20:02:52 -0800

    Add files via upload

--- landscape.js
+++ landscape.js
@@ -10,11 +10,12 @@ function setup() {
 function draw() {
 
     // start off with a background
-    background(147, 183, 207,128)
+    background(162, 176, 171,128)
 
     stroke(220, 230, 245)
 
 strokeWeight(.5)
+
 
 beginShape()
 fill(192, 196, 145,200)
@@ -99,8 +100,25 @@ vertex(569,159)
 vertex(0,192)
 endShape()
 
+
 beginShape()
-fill(110, 102, 150,50)
+fill(255,47,0,90)
+rect(0,75,800,75)
+endShape()
+
+beginShape()
+fill(0,0,0,120)
+rect(0,0,800,75)
+
+endShape()
+
+beginShape()
+fill(255, 204, 0, 75)
+circle(400,75,75)
+endShape()
+
+beginShape()
+fill(110, 102, 150,100)
 curveVertex(800,0)
 curveVertex(800,0)
 curveVertex(580,0)
@@ -109,6 +127,52 @@ curveVertex(800,3)
 curveVertex(800,0)
 endShape()
 
+beginShape()
+fill(188, 189, 168)
+curveVertex(800,500)
+curveVertex(800,500)
+curveVertex(796,458)
+curveVertex(796,458)
+curveVertex(592,470)
+curveVertex(424,442)
+curveVertex(318,376)
+curveVertex(166,374)
+curveVertex(112,492)
+curveVertex(116,500)
+curveVertex(800,500)
+endShape()
+
+beginShape()
+fill(159, 189, 152)
+curveVertex(700,178)
+curveVertex(700,178)
+curveVertex(700,214)
+curveVertex(398,290)
+curveVertex(260,250)
+curveVertex(168,216)
+curveVertex(0,226)
+curveVertex(2,190)
+curveVertex(560,147)
+curveVertex(690,169)
+curveVertex(700,178)
+endShape()
+
+beginShape()
+fill(115, 73, 37)
+curveVertex(728,464)
+curveVertex(688,498)
+curveVertex(714,408)
+curveVertex(696,370)
+curveVertex(706,366)
+curveVertex(718,382)
+curveVertex(792,328)
+curveVertex(772,366)
+curveVertex(794,368)
+curveVertex(762,386)
+curveVertex(702,474)
+curveVertex(752,492)
+curveVertex(728,464)
+endShape()
 
 
 
